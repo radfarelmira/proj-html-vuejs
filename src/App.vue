afd668a121a2095b3c18270e340cadbce269f089
radfarelmira <radfar.elmira@gmail.com>
2022-01-17 21:59:36 +0100

    add banner componenet

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Header :headerToplinksArray="headerTopLinks" :headerTopIconsArray="headerTopIcons" :headerBottomLinksArray="headerBottomLinks"/>
 
     <main>
-      Main
+      <Banner />
     </main>
 
     <Footer />
@@ -13,12 +13,14 @@
 <script>
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
+import Banner from "./components/Banner.vue";
 
 export default {
   name: "App",
   components: {
     Header,
-    Footer
+    Footer,
+    Banner
   },
   data: function() {
     return {
---
+++ src/components/Banner.vue
@@ -0,0 +1,57 @@
+<template>
+  <section>
+      <div class="row">
+        <div class="parallelogram">
+        </div>
+        <div class="sm-container">
+            <div class="banner-content">
+                <h2>banner</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut eum deserunt unde dolore distinctio dolores odio id debitis, corrupti architecto repellendus facilis molestias. Quo, dolore id labore saepe aspernatur inventore?</p>
+            </div>
+        </div>
+      </div>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'Banner',
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/variables.scss';
+
+section{
+    margin-top: 20px;
+    background-color:$primary_color ;
+    color: white;
+
+    .row{
+        height: 300px;
+        background-color: $primary_color;
+        background-image: url(../assets/img/slide-1.jpg);
+        background-repeat: no-repeat;
+        background-position: right;
+        background-size: contain;
+        position: relative;
+        
+
+        .parallelogram{
+            width: 150px;
+            height: 300px;
+            transform: skew(20deg);
+            background: $primary_color;
+            position: absolute;
+            top: 0;
+            left:650px;
+        }
+        
+        .banner-content{
+            z-index: 1;
+            width: 50%;
+        }
+    }
+}
+
+</style>
